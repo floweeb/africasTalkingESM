6fd0a702f4d16f3e062bbe2a7ff446eae8495364
floweeb <floydqaranja@gmail.com>
2025-10-24 18:36:24 +0300

    [feat] implementation of the base payload so as not to think about username or if it's production to add a `senderId`

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,16 @@ import type { SMSResponse } from './interfaces.js';
 
 class AfricasTalkingESM implements AfricasTalkingBlueprint {
     productionApp: boolean
+
     username: string
     apiKey: string
     baseURL: string
+
+    payload: { username: string; senderId?: string; };
+    // base axios instance extending the base blueprint up to you to implement yours
     api: AxiosInstance
 
-    constructor(username: string, apiKey: string){
+    constructor(username: string, apiKey: string, senderId? : string){
         this.productionApp = username !== 'sandbox'
         // credentials for the API
         this.username = username;
@@ -18,6 +22,20 @@ class AfricasTalkingESM implements AfricasTalkingBlueprint {
         this.baseURL = this.productionApp ?            
             'https://api.africastalking.com/version1' :
             'https://api.sandbox.africastalking.com/version1'
+
+        this.payload = {
+            username,
+        }
+
+        if(this.productionApp) {
+            if(!senderId){
+                throw new Error("Production apps require a 'senderId'")
+            }
+            this.payload = {
+                ...this.payload,
+                senderId
+            }
+        }
         // base axios instance
         this.api = axios.create({
             baseURL: this.baseURL,
@@ -33,7 +51,7 @@ class AfricasTalkingESM implements AfricasTalkingBlueprint {
     
     async sms(message: string, phoneNumber: string): Promise<SMSResponse>{
         const payload = {
-            username: this.username,
+            ...this.payload,
             message,
             to: phoneNumber
         }
@@ -52,7 +70,7 @@ class AfricasTalkingESM implements AfricasTalkingBlueprint {
             throw new Error('This method smsBulk() isn\'t supported in sandbox mode yet!')
         }
         const payload = {
-            username: this.username,
+            ...this.payload,
             message,
             phoneNumbers: phoneNumber
         }
